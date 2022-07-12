85a29c6b7e91ac7faeb4cc967e955acc64ed8047
2018008068 <aza1200@naver.com>
2022-07-12 22:21:43 +0900

    github 배포

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var handleMouseClick = (event) =>{
-    clickedCard = document.getElementById(event.target.parentElement.id);
+    if (event.target.tagName == "IMG" ){
+        clickedCard = document.getElementById(event.target.parentNode.parentNode.id);
+    }
+    else if(event.target.tagName == "DIV"){
+        clickedCard = document.getElementById(event.target.parentNode.id);
+    }
     clickedCard.classList.toggle('flipped');
 }
 
@@ -9,13 +14,13 @@ Array.from(document.getElementsByClassName("card")).forEach(
     }
 );
 
-function loadUrls() {
+async function loadUrls() {
     const imageUrls = []
     for (var i = 0; i < 6; i++) {
-        fetch("https://api.thecatapi.com/v1/images/search")
+        await fetch("https://api.thecatapi.com/v1/images/search")
             .then((response) => response.json())
             .then((data) => {
-                imageUrls.push(data[0].url)
+              imageUrls.push(data[0].url)
             })
     }
     return imageUrls
@@ -23,18 +28,26 @@ function loadUrls() {
 
 async function main(){
     const imageUrls = await loadUrls()
-    console.log(imageUrls)
+    console.log(imageUrls);
+    const randomArray = ["1","2","3","4","5","6","7","8","9","10","11","12"]
+    randomArray.sort(()=>Math.random()-0.5)
+    console.log(randomArray,imageUrls)
+    await randomArray
+    for(var idx = 1 ; idx<=6 ; idx++){
+        pairOneId = randomArray[idx*2-1].toString()
+        pairTwoId = randomArray[idx*2-2].toString()
+        console.log(pairTwoId,pairOneId)
+
+        firstCard = document.getElementById(pairOneId)
+        firstCard.classList.add(idx)
+        secondCard = document.getElementById(pairTwoId);
+        secondCard.classList.add(idx)
+
+        secondCard.querySelector(".back img").setAttribute('src',imageUrls[idx-1]);
+        firstCard.querySelector(".back img").setAttribute('src',imageUrls[idx-1]);
+    }
+
 }
 main()
-
-// async function getImageUrl() {
-//     let response = await fetch("https://api.thecatapi.com/v1/images/search")
-//     if (response.ok) {
-//         let json = await response.json();
-//         console.log(json[0].url)
-//     } else {
-//         alert("HTTP-Error " + response.status)
-//     }
-// }
 
 
